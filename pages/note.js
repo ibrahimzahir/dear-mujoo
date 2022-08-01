671e111206e243d36c9001298596f1e56416bcb7
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2022-08-01 20:37:46 +0500

    note page

--- pages/note.js
+++ pages/note.js
@@ -3,63 +3,93 @@ import React from "react";
 function note() {
   return (
     <div
-      className="relative py-16
-               bg-black before:absolute before:inset-0 before:w-full before:h-[50%] before:bg-gray-200"
+      className="relative py-32
+               bg-black before:absolute before:inset-0 before:w-full before:h-[50%] before:bg-black"
     >
       <div className="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
-        <div className="m-auto space-y-8 md:w-8/12 lg:w-full">
-          <div className="rounded-xl border bg-opacity-50 backdrop-blur-2xl bg-white shadow-xl">
-            <div className="lg:grid lg:grid-cols-2">
-              <div className="p-6 sm:p-16">
-                <h2 className="mb-8 text-2xl text-cyan-900 font-bold">
-                  Sign in to your account
-                </h2>
-                <form action="" className="space-y-8">
-                  <div className="space-y-2">
-                    <label htmlFor="email" className="text-gray-700">
-                      Email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
-        focus:ring-2 focus:ring-sky-300 focus:outline-none
-        invalid:ring-2 invalid:ring-red-400"
-                    />
-                  </div>
-
-                  <div>
-                    <div className="flex items-center justify-between">
-                      <label htmlFor="pwd" className="text-gray-700">
-                        Password
-                      </label>
-                      <button className="p-2 -mr-2" type="reset">
-                        <span className="text-sm text-sky-500">
-                          Forgot your password ?
-                        </span>
-                      </button>
-                    </div>
-                    <input
-                      type="password"
-                      name="pwd"
-                      id="pwd"
-                      className="block w-full px-4 py-3 rounded-md border border-gray-300 text-gray-600 transition duration-300
-                                            focus:ring-2 focus:ring-sky-300 focus:outline-none
-                                            invalid:ring-2 invalid:ring-red-400"
-                    />
-                  </div>
-
-                  <button
-                    type="submit"
-                    className="w-full py-3 px-6 rounded-md bg-sky-600
-                                        focus:bg-sky-700 active:bg-sky-500"
-                  ></button>
-                </form>
+        <form>
+          <div className="mb-4 w-full bg-gray-50 rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600">
+            <div className="py-2 px-4 bg-white rounded-t-lg dark:bg-gray-800">
+              <label htmlFor="comment" className="sr-only">
+                Your comment
+              </label>
+              <textarea
+                id="comment"
+                rows="4"
+                className="px-0 w-full text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                placeholder="Write a comment..."
+                required
+              ></textarea>
+            </div>
+            <div className="flex justify-between items-center py-2 px-3 border-t dark:border-gray-600">
+              <button
+                type="submit"
+                className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+              >
+                Post comment
+              </button>
+              <div className="flex pl-0 space-x-1 sm:pl-2">
+                <button
+                  type="button"
+                  className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Attach file</span>
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Set location</span>
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex justify-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                >
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="sr-only">Upload image</span>
+                </button>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
